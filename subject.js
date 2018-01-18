64163a3f522649514f3e2987e3b03ff8f5143c04
David Alsh <alshdavid@users.noreply.github.com>
2018-01-18 23:58:06 +1100

    Update subject.js

--- subject.js
+++ subject.js
@@ -1,30 +1,29 @@
-class Subject {
-    constructor(){
-      this.subscribers = []
+export class Subject {
+    constructor() {
+        this.subscribers = []
     }
-    
+
     subscribe(cb) {
-      let subscription = {
-        callback: cb,
-        unsubscribe: () => { 
-          subscription.callback = null 
-          this.cleanup()
+        let subscription = {
+            callback: cb,
+            unsubscribe: () => {
+                subscription.callback = null
+                this.cleanup()
+            }
         }
-      }
-      this.subscribers.push(subscription)
-      return subscription
+        this.subscribers.push(subscription)
+        return subscription
     }
-    
+
     next(val) {
-      this.subscribers.forEach(
-        sub => sub.callback ? sub.callback(val) : null
-      )
+        this.subscribers.forEach(
+            sub => (sub.callback ? sub.callback(val) : null)
+        )
     }
-    
+
     cleanup() {
-      this.subscribers = this.subscribers
-        .filter(
-          sub => sub.callback ? sub.callback : null
+        this.subscribers = this.subscribers.filter(
+            sub => (sub.callback ? sub.callback : null)
         )
     }
 }
