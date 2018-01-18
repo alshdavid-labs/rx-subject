6acb6898389c177734b63025954fc8f7e69d0a8a
David Alsh <alshdavid@users.noreply.github.com>
2018-01-18 23:55:13 +1100

    Create subject-module.js

---
+++ subject-module.js
@@ -0,0 +1,30 @@
+export class Subject {
+    constructor(){
+      this.subscribers = []
+    }
+    
+    subscribe(cb) {
+      let subscription = {
+        callback: cb,
+        unsubscribe: () => { 
+          subscription.callback = null 
+          this.cleanup()
+        }
+      }
+      this.subscribers.push(subscription)
+      return subscription
+    }
+    
+    next(val) {
+      this.subscribers.forEach(
+        sub => sub.callback ? sub.callback(val) : null
+      )
+    }
+    
+    cleanup() {
+      this.subscribers = this.subscribers
+        .filter(
+          sub => sub.callback ? sub.callback : null
+        )
+    }
+}
